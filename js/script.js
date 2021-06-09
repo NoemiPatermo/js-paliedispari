5f71e9247aa1aea269065eb8f1966972e14c82e5
noemi86-cpu <noemipatermo@gmail.com>
2021-06-09 16:30:42 +0200

    palindroma

--- js/script.js
+++ js/script.js
@@ -2,21 +2,29 @@
 // Creare una funzione per capire se la parola inserita è palindroma
 
 //parola palindroma = parola che può essere letta da sinistra a destra e viceversa (osso, otto, anna)
-var parola =  "cane"; //prompt("Inserisci una parola");
-//applica il metodo split a parola
+
+var parola =  prompt("Inserisci una parola");
+
+/*applica il metodo split alla tua variabile
 var parolaDivisa = parola.split("");//  sono un array
-console.log(parolaDivisa);// avrò un array di 4 elementi
+//console.log(parolaDivisa);// avrò un array di 4 elementi
 
 // usa il metodo reverse per invertire array
 var parolaDivisa = parolaDivisa.reverse()
 
  // ora riunisci le lettere per avere la stringa e lo fai con join
  var parolaInvertita = parolaDivisa.join("");
- console.log(parolaInvertita);
+ //console.log(parolaInvertita);*/
 
- // confronta la parola iniziale con quella invertita per verificare se è palindroma 
- if(parola === parolaInvertita){
-     console.log("la parola è palindroma");
- }else{
-     console.log("la parola non è palindroma");
+ function parolaPalindroma(str){
+     var strReverse = str.split('').reverse('').join('');
+     return strReverse;
  }
+ var parolaInvertita = parolaPalindroma(parola);
+ if(parola === parolaInvertita){
+     document.getElementById("palindroma").innerText = "Va bene, hai inserito una parola palindroma! Leggimi!"
+ }else{
+    document.getElementById("palindroma").innerText = "La prola inserita non è palindroma!"
+ }
+
+ 
