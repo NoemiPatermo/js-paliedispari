c1a4f7279d6d301724bdd5f86e6850184e0abe5d
noemi86-cpu <noemipatermo@gmail.com>
2021-06-09 18:16:56 +0200

    palindroma

--- js/script.js
+++ js/script.js
@@ -3,22 +3,20 @@
 
 //parola palindroma = parola che può essere letta da sinistra a destra e viceversa (osso, otto, anna)
 
-var parola =  prompt("Inserisci una parola");
-
 /*applica il metodo split alla tua variabile
-var parolaDivisa = parola.split("");//  sono un array
-//console.log(parolaDivisa);// avrò un array di 4 elementi
-
-// usa il metodo reverse per invertire array
-var parolaDivisa = parolaDivisa.reverse()
+var parolaDivisa = parola.split("");
+// sono un array adesso
+                                                                // usa il metodo reverse per invertire array
+                                                               var parolaDivisa = parolaDivisa.reverse()
 
  // ora riunisci le lettere per avere la stringa e lo fai con join
  var parolaInvertita = parolaDivisa.join("");
  //console.log(parolaInvertita);*/
+ var parola =  prompt("Inserisci una parola");
 
- function parolaPalindroma(str){
-     var strReverse = str.split('').reverse('').join('');
-     return strReverse;
+ function parolaPalindroma(parola){
+     var stringa = parola.split('').reverse('').join('');
+     return stringa;
  }
  var parolaInvertita = parolaPalindroma(parola);
  if(parola === parolaInvertita){
@@ -27,4 +25,9 @@ var parolaDivisa = parolaDivisa.reverse()
     document.getElementById("palindroma").innerText = "La prola inserita non è palindroma!"
  }
 
- 
+ //Pari e Dispari
+//L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+//Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+//Sommiamo i due numeri.
+//Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+//Infine, dichiariamo chi ha vinto.
